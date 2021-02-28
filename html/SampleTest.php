d1cf828beb67db21696c0b0d5d7556fa254d7cf5
sachiko-kame <sachiko-kame@users.noreply.github.com>
2021-02-28 22:34:33 +0900

    再度入れ直しての

--- html/SampleTest.php
+++ html/SampleTest.php
@@ -5,17 +5,17 @@ require_once 'sample.php';
 
 class SampleTest extends TestCase
 {
-    // public function testNoA()
-    // {
-    //     //同じ値、同じ型でない場合エラー
-    //     $this->assertSame('2204', 2204);
-    // }
-    //
-    // public function testOkA()
-    // {
-    //     //同じ値、同じ型でない場合エラー
-    //     $this->assertSame('2204', '2204');
-    // }
+    public function testNoA()
+    {
+        //同じ値、同じ型でない場合エラー
+        $this->assertSame('2204', 2204);
+    }
+
+    public function testOkA()
+    {
+        //同じ値、同じ型でない場合エラー
+        $this->assertSame('2204', '2204');
+    }
 
     public function testSample()
     {
